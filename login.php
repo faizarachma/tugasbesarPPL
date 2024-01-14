d80f864aaac162009a3ca07ee739dee400872a24
faizarachma_ <20102110@ittelkom-pwt.ac.id>
2024-01-14 22:10:53 +0700

    Add

--- login.php
+++ login.php
@@ -1,3 +1,40 @@
+<?php
+include 'koneksi.php';
+
+// Ambil data dari formulir login
+$email = $_POST['email'];
+$password = $_POST['password'];
+
+// Query untuk memeriksa data pengguna
+$query = "SELECT * FROM users WHERE email = '$email'";
+$result = $conn->query($query);
+
+// Periksa apakah pengguna ditemukan
+if ($result->num_rows > 0) {
+    $row = $result->fetch_assoc();
+    $hashedPassword = $row['password'];
+
+    // Periksa apakah kata sandi cocok
+    if (password_verify($password, $hashedPassword)) {
+        // Otentikasi berhasil, atur sesi atau tindakan lain yang diperlukan
+        session_start();
+        $_SESSION['user_id'] = $row['id'];
+        // Redirect ke halaman lain setelah login berhasil
+        header('Location: form.html');
+        exit();
+    } else {
+        // Password tidak cocok, beri tahu pengguna
+        echo "Password tidak benar";
+    }
+} else {
+    // Pengguna tidak ditemukan, beri tahu pengguna
+    echo "Email tidak terdaftar";
+}
+
+// Tutup koneksi database
+$conn->close();
+?>
+
 <!DOCTYPE html>
 <html lang="en">
   <head>
@@ -65,3 +102,4 @@
     </section>
   </body>
 </html>
+
